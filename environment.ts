e8b97aeab439fa6ff50289b1f3f5bd23f157ce37
kumarsheetal <103719163+kumarsheetal@users.noreply.github.com>
2022-04-15 09:35:12 +0530

    Add files via upload

---
+++ environment.ts
@@ -0,0 +1,39 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  count:1126,
+  next:"https://pokeapi.co/api/v2/pokemon/?offset=20&limit=20",
+  previous: null,
+  production: false,
+  apiUrl:["https://pokeapi.co/api/v2/pokemon/1/",
+  "https://pokeapi.co/api/v2/pokemon/2/",
+  "https://pokeapi.co/api/v2/pokemon/3/",
+  "https://pokeapi.co/api/v2/pokemon/4/",
+  "https://pokeapi.co/api/v2/pokemon/5/",
+  "https://pokeapi.co/api/v2/pokemon/6/",
+  "https://pokeapi.co/api/v2/pokemon/7/",
+  "https://pokeapi.co/api/v2/pokemon/8/",
+  "https://pokeapi.co/api/v2/pokemon/9/",
+  "https://pokeapi.co/api/v2/pokemon/10/",
+  "https://pokeapi.co/api/v2/pokemon/11/",
+  "https://pokeapi.co/api/v2/pokemon/12/",
+  "https://pokeapi.co/api/v2/pokemon/13/",
+  "https://pokeapi.co/api/v2/pokemon/14/",
+  "https://pokeapi.co/api/v2/pokemon/15/",
+  "https://pokeapi.co/api/v2/pokemon/16/",
+  "https://pokeapi.co/api/v2/pokemon/17/",
+  "https://pokeapi.co/api/v2/pokemon/18/",
+  "https://pokeapi.co/api/v2/pokemon/19/",
+  "https://pokeapi.co/api/v2/pokemon/20/"]
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
